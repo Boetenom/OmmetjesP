dc88dbcb4de39314462652311cd147e242ce1503
Boetenom <bpboetenom@gmail.com>
2025-07-04 11:31:00 +0200

    Add files via upload

---
+++ styles/Parkeren_7_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_Parkeren_7 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("P") !== null) {
+        labelText = String(feature.get("P"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0],  fill: new ol.style.Fill({color: 'rgba(86,147,217,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
